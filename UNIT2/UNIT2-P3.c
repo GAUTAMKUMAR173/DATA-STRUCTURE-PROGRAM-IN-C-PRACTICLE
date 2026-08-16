a18918c3394fd18aedba7f080725f5879522f471
Gautam Kumar <roy12ff1998@gmail.com>
2026-08-16 18:20:50 +0530

    Add stack implementation for factorial calculation
    Implement stack-based factorial calculation.

---
+++ UNIT2/UNIT2-P3.c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+#define MAX 30
+
+int stack[MAX],top=-1;
+void push(int ch);
+int pop();
+
+void main()
+{
+
+
+    int i=0,n,fact=1;
+
+
+    printf("\n enter value:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+
+
+
+        push(i);
+
+
+    }
+
+    while(top!=-1){
+
+
+        fact=fact*pop();
+    }
+
+    printf("\n factorial is: %d",fact);
+    return 0;
+}
+
+void push(int value)
+{
+
+    if(top==MAX-1)
+    {
+
+
+        printf("\ stack overflow,,");
+
+    }
+
+    else
+    {
+
+        top++;
+        stack[top]=value;
+    }
+
+    }
+
+    int pop()
+    {
+
+        int val;
+        if(top==-1)
+        {
+
+            printf("\n stack is underflow--");
+            return -1;
+
+        }
+
+        else
+
+
+    {
+
+
+
+    val=stack[top];
+    top--;
+
+    return val;
+    }
+
+    }
+

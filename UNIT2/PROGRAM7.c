a9259ca13d3db1f80362e6722a4c2fdf76184664
Gautam Kumar <roy12ff1998@gmail.com>
2026-08-17 23:05:22 +0530

    Add stack implementation for smallest common divisor
    Implement a stack to find the smallest common divisor of a number.

---
+++ UNIT2/PROGRAM7.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#define MAX 100
+int stack[MAX];
+int top = -1;
+
+int num;
+void push(int value);
+int pop();
+
+int main()
+{
+ int i, divisor;
+
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+
+
+    for (i = 2; i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            push(i);
+            printf("Smallest Common Divisor = %d\n", i);
+            break;
+        }
+    }
+
+}
+
+void push(int value)
+{
+    if (top == MAX - 1)
+    {
+        printf("Stack Overflow\n");
+    }
+    else
+        stack[++top] = value;
+
+}
+
+int pop()
+{
+    if (top == -1)
+    {
+        printf("Stack Underflow\n");
+        return -1;
+    }
+    else
+    {
+        return stack[top--];
+    }
+}
